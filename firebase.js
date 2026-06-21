d0279acc8b640ec17f32386f6bbe94bc4bd81f0e
weifan168188-blip <weifan168188@gmail.com>
2026-06-21 11:28:55 +0800

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,19 +1,16 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
-import { getStorage } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-storage.js";
+// Firebase CDN Version
 
 const firebaseConfig = {
   apiKey: "AIzaSyA394LWxxX3_mjiiKQU5GIWTGrWSHmCVfU",
   authDomain: "lion-credit-system.firebaseapp.com",
   projectId: "lion-credit-system",
-  storageBucket: "lion-credit-system.firebasestorage.app",
+  storageBucket: "lion-credit-system.appspot.com",
   messagingSenderId: "720399842142",
   appId: "1:720399842142:web:b2635c6228553c28eaedcf"
 };
 
-const app = initializeApp(firebaseConfig);
+firebase.initializeApp(firebaseConfig);
 
-export const auth = getAuth(app);
-export const db = getFirestore(app);
-export const storage = getStorage(app);
+const db = firebase.firestore();
+const storage = firebase.storage();
+const auth = firebase.auth();
